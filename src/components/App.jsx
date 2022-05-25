88452879c10e877d4a2d93ca2ef98590c0710812
yongseongjeon <yongseong.jeon95@gmail.com>
2022-05-25 14:12:54 +0900

    💄 Modal 컴포넌트 뷰 생성

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Modal from 'components/common/Modal';
+
+function App() {
+  return (
+    <div className="App">
+      <Modal isDim position="CENTER" padding="30px" borderRadius="30px" />
+    </div>
+  );
+}
+
+export default App;
