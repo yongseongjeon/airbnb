8f7fa620130bd30bf5a2c31d195497a7ae159eaa
yongseongjeon <yongseong.jeon95@gmail.com>
2022-05-31 03:23:05 +0900

    ✨ 캘린더 선택된 날짜 배경 색 및 글씨색 변경

--- src/components/calendar/Day.jsx
+++ src/components/calendar/Day.jsx
@@ -10,9 +10,29 @@ function Day({ year, month, day }) {
   const curDateTime = new Date().getTime();
   const isDatePast = curDateTime > calendarDateTime;
   const { checkIn, setCheckIn, checkOut, setCheckOut } = useContext(FilterContext);
+  const isSelected =
+    isEqualDate(checkIn, { year, month, day }) || isEqualDate(checkOut, { year, month, day });
+
+  function isEqualDate(date1, date2) {
+    if (date1?.year !== date2.year) {
+      return false;
+    }
+    if (date1?.month !== date2.month) {
+      return false;
+    }
+    if (date1?.day !== date2.day) {
+      return false;
+    }
+    return true;
+  }
 
   return (
-    <StyledDay disabled={isDatePast} isDatePast={isDatePast} onClick={handleClickedDay}>
+    <StyledDay
+      disabled={isDatePast}
+      isSelected={isSelected}
+      isDatePast={isDatePast}
+      onClick={handleClickedDay}
+    >
       {day}
     </StyledDay>
   );
@@ -55,8 +75,10 @@ const StyledDay = styled.button`
   margin: auto auto;
   font-weight: ${FONT.WEIGHT.MEDIUM};
   border-radius: 30px;
-  color: ${({ isDatePast }) => (isDatePast ? COLOR.GREY[300] : COLOR.GREY[600])};
+  color: ${({ isDatePast }) => isDatePast && COLOR.GREY[300]};
+  color: ${({ isSelected }) => isSelected && COLOR.WHITE};
+  background-color: ${({ isSelected }) => isSelected && COLOR.GREY[600]};
   :hover {
-    border: ${({ isDatePast }) => (isDatePast ? '' : `1px solid ${COLOR.GREY[600]}`)};
+    border: ${({ isDatePast }) => !isDatePast && `1px solid ${COLOR.GREY[600]}`};
   }
 `;
