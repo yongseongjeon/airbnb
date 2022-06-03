c17027ddc32f0c06b8421d178c81ce5a6a35cd31
ju-kkim <wndus8198@naver.com>
2022-06-03 14:14:13 +0900

    ✨ map  초기 위치 변경, 마커 표시

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -9,10 +9,15 @@ function Map() {
   useEffect(function loadKakaoMap() {
     const container = document.getElementById('map');
     const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      center: new kakao.maps.LatLng(37.490821, 127.033417),
       level: 3,
     };
     const map = new kakao.maps.Map(container, options);
+    const markerPosition = new kakao.maps.LatLng(37.490821, 127.033417);
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+    marker.setMap(map);
   }, []);
 
   return (
