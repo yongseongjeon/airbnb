0e9d732a7143ea5a7068e950864d17ade0e0e776
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-08 17:43:38 +0900

    ✨ AccommodationContext 생성

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,21 @@ import Home from 'pages/Home';
 import SearchResult from 'pages/SearchResult';
 import ReservationSuccess from 'pages/ReservationSuccess';
 import NoMatch from 'pages/NoMatch';
+import { AccommodationProvider } from 'store/AccommodationContext';
 
 function App() {
   return (
     <FilterProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path="/searchResult" element={<SearchResult />} />
-          <Route path="/reservationSuccess" element={<ReservationSuccess />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </BrowserRouter>
+      <AccommodationProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route path="/searchResult" element={<SearchResult />} />
+            <Route path="/reservationSuccess" element={<ReservationSuccess />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </BrowserRouter>
+      </AccommodationProvider>
     </FilterProvider>
   );
 }
---
+++ src/store/AccommodationContext.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable react/jsx-no-constructed-context-values */
+
+import ACCOMMODATIONS from 'mockData/accommodation';
+import { createContext, useContext, useEffect, useState } from 'react';
+import pipe from 'utils/util';
+import { FilterContext } from './FilterContext';
+
+const AccommodationContext = createContext();
+
+function AccommodationProvider({ children }) {
+  const [accommodations, setAccommodations] = useState([]);
+  const { schedule, priceSlider, guest } = useContext(FilterContext);
+  const { checkIn, checkOut } = schedule;
+  const { low, high } = priceSlider.price;
+  const { adult, child, infant } = guest;
+
+  useEffect(filterAccommodations, [schedule, priceSlider, guest]);
+
+  return (
+    <AccommodationContext.Provider value={{ accommodations }}>
+      {children}
+    </AccommodationContext.Provider>
+  );
+
+  function filterAccommodations() {
+    const filteredAccommodations = pipe(filterSchedule, filterPrice, filterGuest)(ACCOMMODATIONS);
+    setAccommodations(filteredAccommodations);
+
+    function filterSchedule(accommodations) {
+      return accommodations.filter(canCheckIn);
+    }
+
+    function canCheckIn(accommodation) {
+      const hasSchedule = checkIn && checkOut;
+      if (!hasSchedule) {
+        return;
+      }
+
+      const checkInDate = new Date(checkIn.year, checkIn.month - 1, checkIn.day);
+      const checkOutDate = new Date(checkOut.year, checkOut.month - 1, checkOut.day);
+      const diff = checkOutDate - checkInDate;
+      const baseOfDay = 24 * 60 * 60 * 1000;
+      const diffDay = diff / baseOfDay;
+      return accommodation.maxStay <= diffDay;
+    }
+
+    function filterPrice(accommodations) {
+      return accommodations.filter(isInnerRange);
+    }
+
+    function isInnerRange(accommodation) {
+      const isOverLowPrice = accommodation.price >= low;
+      const isUnderHighPrice = accommodation.price <= high;
+      return isOverLowPrice && isUnderHighPrice;
+    }
+
+    function filterGuest(accommodations) {
+      return accommodations.filter(isUnderMaxGuest);
+    }
+
+    function isUnderMaxGuest(accommodation) {
+      const totalGuest = adult + child + infant;
+      return accommodation.maxGuest >= totalGuest;
+    }
+  }
+}
+
+export { AccommodationContext, AccommodationProvider };
