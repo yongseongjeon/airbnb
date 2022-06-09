09fbce5848dd5c9bcbc278db1a47669c173b70f4
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-09 16:27:06 +0900

    🐛 필터값 입력 없이 검색했을 때 데이터 랜더링

--- src/components/SearchBar/MiniSearchBar.jsx
+++ src/components/SearchBar/MiniSearchBar.jsx
@@ -4,11 +4,13 @@ import { getConvertPriceText, getGuestText, getScheduleText } from 'utils/fromSt
 import styled from 'styled-components';
 import COLOR from 'styles/colors';
 import FONT from 'styles/font';
+import { AccommodationContext } from 'store/AccommodationContext';
 import SearchButton from './SearchButton';
 
 function MiniSearchBar({ setSpreadSearchBar }) {
   const { modalDispatch, schedule, scheduleDispatch, priceSlider, guest, guestDispatch } =
     useContext(FilterContext);
+  const { filterAccommodations } = useContext(AccommodationContext);
   const { checkIn, checkOut } = schedule;
   const { low, high } = priceSlider.price;
   const { adult, child, infant } = guest;
@@ -21,6 +23,8 @@ function MiniSearchBar({ setSpreadSearchBar }) {
     setDefaultSchedule();
     setDefaultGuest();
   }, []);
+
+  useEffect(filterAccommodations, [schedule, guest]);
 
   return (
     <Container
